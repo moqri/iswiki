ebc749b4ee7212ba8baec1af4b6d9a6abdb938f1
Ubuntu <ubuntu@ip-172-31-53-98.ec2.internal>
2017-01-13 18:27:05 +0000

    .

--- import.py
+++ import.py
@@ -1,3 +1,14 @@
+from pywikibot.data import api
+import pywikibot
+import pprint
+def getItems(site, itemtitle):
+        params = { 'action' :'wbsearchentities' , 'format' : 'json' , 'language' : 'en', 'type' : 'item', 'search': itemtitle}
+        request = api.Request(site=site,**params)
+        return request.submit()
+site = pywikibot.Site()
+repo = site.data_repository()
+
+
 import pandas as pd
 df=pd.read_csv("articles_2010.csv")
 for i in range(1):
@@ -10,3 +21,9 @@ for author in authors_list:
     last,first= author.strip().split(',')
     author_name=first +" "+ last
     print author_name
+    wikidataEntries = getItems(site, author_name)   
+    if len(wikidataEntries['search']) == 0 :
+     data = {'labels': {'en': author_name}}
+     item = pywikibot.ItemPage(repo)
+     item.editEntity(data, summary=u'Edit item')
+ 
--- item.py
+++ item.py
@@ -1,8 +1,17 @@
+from pywikibot.data import api
 import pywikibot
+import pprint
+
+def getItems(site, itemtitle):
+        params = { 'action' :'wbsearchentities' , 'format' : 'json' , 'language' : 'en', 'type' : 'item', 'search': itemtitle}
+        request = api.Request(site=site,**params)
+        return request.submit()
+
 site = pywikibot.Site()
 repo = site.data_repository()
 item = pywikibot.ItemPage(repo)
+wikidataEntries = getItems(site, "author 102")
+print wikidataEntries
 data = {'labels': {'en': 'paper 102'}}
-item.editEntity(data, summary=u'Edit item')
-
-print item.get()
+#item.editEntity(data, summary=u'Edit item')
+#print item.get()
