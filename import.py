b029957ced0def77440201b739baa0af940107c0
Ubuntu <ubuntu@ip-172-31-53-98.ec2.internal>
2017-01-13 22:31:49 +0000

    .

--- import.py
+++ import.py
@@ -8,22 +8,53 @@ def getItems(site, itemtitle):
 site = pywikibot.Site()
 repo = site.data_repository()
 
-
-import pandas as pd
-df=pd.read_csv("articles_2010.csv")
-for i in range(1):
-    row=next(df.iterrows())[1]
-    title=row['TI']
-    authors=row['AF']
-
-authors_list=authors.split(';')
-for author in authors_list:
-    last,first= author.strip().split(',')
-    author_name=first +" "+ last
-    print author_name
-    wikidataEntries = getItems(site, author_name)   
+def addAuthor(author_name):
+    wikidataEntries = getItems(site, author_name)
     if len(wikidataEntries['search']) == 0 :
      data = {'labels': {'en': author_name}}
      item = pywikibot.ItemPage(repo)
-     item.editEntity(data, summary=u'Edit item')
+     item.editEntity(data, summary=u'new author')
+
+def addArticle(title):
+    wikidataEntries = getItems(site, title)
+    if len(wikidataEntries['search']) == 0 :
+     data = {'labels': {'en': title}}
+     item = pywikibot.ItemPage(repo)
+     item.editEntity(data, summary=u'new article')
+
+def addAuthorStatement(article_item_page,author_name):
+  claim = pywikibot.Claim(repo, u'P2')
+  author_item = getItems(site, author_name)
+  author_id= author_item['search'][0]['id']
+  target = pywikibot.ItemPage(repo, author_id)
+  claim.setTarget(target)
+  article_item_page.addClaim(claim)
+
+import pandas as pd
+df=pd.read_csv("articles_2010.csv")
+for i, row in df.iterrows():
+ if i > 1000 :
+     break
+ title=row['TI']
+ print title
+ addArticle(title)
+
+ authors=row['AF']
+ authors_list=authors.split(';')
+ article_item = getItems(site, title)
+ article_id= article_item['search'][0]['id']
+ print article_id
  
+ for author in authors_list:
+  author_name_split=reversed(author.strip().split(','))
+  author_name=' '.join(name.strip() for name in author_name_split)
+  print author_name
+  addAuthor(author_name)
+  article_item_page = pywikibot.ItemPage(repo, article_id)
+  if 'P2' in article_item_page.get()["claims"]:
+   article_author_claims=article_item_page.get()["claims"]['P2']
+   article_author_names=[claim.getTarget().get()['labels']['en'] for claim in article_author_claims] 
+   if (author_name in article_author_names):
+    print 'author statement exists'
+    continue
+  addAuthorStatement(article_item_page,author_name)
